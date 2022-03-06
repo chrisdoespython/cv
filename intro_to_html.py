678ff10e0c75b3a280a7145cc8277327dbe3ab6f
chrisdoespython <101029615+chrisdoespython@users.noreply.github.com>
2022-03-05 22:04:02 -0500

    Add initial cv website files

---
+++ intro_to_html.py
@@ -0,0 +1,36 @@
+# ##################### Resources ##########################
+
+# GitHub Info
+GITHUB = "https://github.com/"
+PW = "ySp5WVe73Exv34r"
+EMAIL = "chrisdoespython@gmail.com"
+USER = "chrisdoespython"
+
+# Editors
+CODE_PEN = "https://codepen.io/"
+ATOM = "https://atom.io/"
+ATOM_PACKAGES = "https://docs.google.com/document/d/e/2PACX-1vQNuhDC5pFXEVVNGasvddKuDHEXnqR033lsSD5tLA9NiEdHrsYM4MXVEXja2RnBgsCxK6XEo6YkMOFI/pub"
+
+# HTML Documentation
+MDN_HEADINGS_DOC = "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Heading_Elements"
+W3_HTML_HEADINGS_DOC = "https://www.w3schools.com/html/html_headings.asp"
+DEV_DOCS_IO = "https://devdocs.io/"
+UTF_8 = "https://www.fileformat.info/info/charset/UTF-8/list.htm"
+UNICODE = "https://unicode-table.com/en/"
+EMMET_CHEAT_SHEET = "https://docs.emmet.io/cheat-sheet/"
+
+# For Fun/Research
+ADVENTURES_OF_SHERLOCK_HOLMES = "https://www.gutenberg.org/ebooks/1661"
+TECH_CRUNCH = "https://techcrunch.com/"
+SUB_CABLES = "https://www.submarinecablemap.com/"
+WAYBACK_MACHINE = "https://archive.org/web/"
+THOMAS_CORMEN = "https://www.cs.dartmouth.edu/~thc/"
+JOHN_KLEINBERG = "http://www.cs.cornell.edu/home/kleinber/"
+EMERITUS_DONALD_KNUTH = "https://www-cs-faculty.stanford.edu/~knuth/"
+JOEL_ON_SOFTWARE = "https://www.joelonsoftware.com/2003/10/08/the-absolute-minimum-every-software-developer-absolutely-positively-must-know-about-unicode-and-character-sets-no-excuses/"
+WIKI_GAME = "https://www.thewikigame.com/"
+PASCAL_VAN_GEMERT = "http://www.pascalvangemert.nl/#/experiences"
+
+# ##########################################################
+
+HTML = "Hyper Text Markup Language"
